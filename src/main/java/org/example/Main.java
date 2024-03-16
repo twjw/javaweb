fd6bd4a125d5b3a039019dfd9def4015c7d33b5c
twjw <aaaawjwa@gmail.com>
2024-03-17 00:11:24 +0800

    mybatis1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.example.entity.Student;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    SqlSessionFactory sqlSessionFactory;
+
+    try (Reader reader = Resources.getResourceAsReader("mybatis-config.xml")) {
+      sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+      try (SqlSession sqlSession = sqlSessionFactory.openSession(true);) {
+        List<Student> studentList = sqlSession.selectList("studentMapper.selectStudent");
+        studentList.forEach(System.out::println);
+
+        Student student = sqlSession.selectOne("studentMapper.selectStudentOne", 1);
+        System.out.println(student);
+      }
+    } catch (IOException e) {
+      throw new ExceptionInInitializerError(e);
+    }
   }
 }
---
+++ src/main/java/org/example/entity/Student.java
@@ -0,0 +1,10 @@
+package org.example.entity;
+
+import lombok.Data;
+
+@Data
+public class Student {
+  int sid;
+  String name;
+  String sex;
+}
